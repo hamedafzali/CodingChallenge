b59224eb671690b174421291529d008325364986
hamed afzali <AFZALI.HAMED@GMAIL.COM>
2021-07-09 10:09:25 +0430

    in frontend Header and classes were implemented

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "../assets/images/logo_crewmeister_white.svg";
+const Header = () => {
+  return (
+    <nav className="navbar">
+      <img src={logo} className="logo" alt="" />
+
+      <a className="navLogin" href="#">
+        Login
+      </a>
+    </nav>
+  );
+};
+
+export default Header;
---
+++ frontend/src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <div className="container p-3 mt-2 border rounded">
+      Funktioniert auf jedem Endgerät Den Urlaubsplaner können Sie auf Ihrem PC,
+      Smartphone oder Tablet verwenden. Der Vorteil: Alle Daten synchronisieren
+      sich automatisch, auch wenn Sie an mehreren Geräten gleichzeitig
+      angemeldet sind.
+    </div>
+  );
+};
+
+export default Main;
